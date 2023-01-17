7593f80a2730070aaeefc0da253c71ef89651f50
Archi-Paweu <p.s.wisniewski@gmail.com>
2023-01-17 19:58:52 +0100

    1

---
+++ ConstructionPrototype/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionPrototype.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult JoinUs()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
