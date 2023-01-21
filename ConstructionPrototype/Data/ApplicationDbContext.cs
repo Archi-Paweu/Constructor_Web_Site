79ea9e47c3809c38848c2524633354a045ac7b15
Archi-Paweu <p.s.wisniewski@gmail.com>
2023-01-21 19:08:52 +0100

    .

--- ConstructionPrototype/Data/ApplicationDbContext.cs
+++ ConstructionPrototype/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ConstructionPrototype.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConstructionPrototype.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
     }
+
+    // public DbSet<Item> Items { get; set; }
+
 }
---
+++ ConstructionPrototype/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConstructionPrototype.Models
+{
+    public class Item
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        [DisplayName("Building Name")]
+        public string BuildingName { get; set; }
+
+    }
+}
